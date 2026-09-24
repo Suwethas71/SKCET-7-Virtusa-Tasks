e22fac975b767759ca28fb3603ed2d6dd15627cf
Suwetha E <suwethas71@gmail.com>
2026-09-24 14:08:08 +0530

    Update integerToRoman.js

--- SUWETHA E/JavaScript/integerToRoman.js
+++ SUWETHA E/JavaScript/integerToRoman.js
@@ -1,50 +1,16 @@
-const readline = require("readline");
+const fs = require("fs");
 
-function intToRoman(num) {
-  if (num <= 0 || num > 3999) {
-    return "Number out of range (1 - 3999)";
-  }
+let n = Number(fs.readFileSync(0, "utf8"));
+let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+let symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
-  const lookup = [
-    { value: 1000, symbol: "M" },
-    { value: 900, symbol: "CM" },
-    { value: 500, symbol: "D" },
-    { value: 400, symbol: "CD" },
-    { value: 100, symbol: "C" },
-    { value: 90, symbol: "XC" },
-    { value: 50, symbol: "L" },
-    { value: 40, symbol: "XL" },
-    { value: 10, symbol: "X" },
-    { value: 9, symbol: "IX" },
-    { value: 5, symbol: "V" },
-    { value: 4, symbol: "IV" },
-    { value: 1, symbol: "I" },
-  ];
+let roman = "";
 
-  let roman = "";
-  let remaining = num;
-
-  for (const item of lookup) {
-    while (remaining >= item.value) {
-      roman += item.symbol;
-      remaining -= item.value;
+for (let i = 0; i < values.length; i++) {
+    while (n >= values[i]) {
+        roman += symbols[i];
+        n -= values[i];
     }
-  }
-
-  return roman;
 }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question("Enter an integer (1 to 3999): ", (answer) => {
-  const val = parseInt(answer, 10);
-  if (isNaN(val)) {
-    console.log("Please enter a valid numeric integer.");
-  } else {
-    console.log(`Roman numeral for ${val}: ${intToRoman(val)}`);
-  }
-  rl.close();
-});
+console.log(roman);
