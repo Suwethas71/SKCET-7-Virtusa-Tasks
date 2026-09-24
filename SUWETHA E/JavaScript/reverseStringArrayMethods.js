3d853f932126814f4af0b8440120f7746a24af09
Suwetha E <suwethas71@gmail.com>
2026-09-24 14:08:39 +0530

    Update reverseStringArrayMethods.js

--- SUWETHA E/JavaScript/reverseStringArrayMethods.js
+++ SUWETHA E/JavaScript/reverseStringArrayMethods.js
@@ -1,19 +1,6 @@
-const readline = require("readline");
+const fs = require("fs");
 
-function reverseUsingArrayMethods(str) {
-  if (typeof str !== "string") {
-    return "";
-  }
-  return str.split("").reverse().join("");
-}
+let str = fs.readFileSync(0, "utf8").trim();
+let rev = str.split("").reverse().join("");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question("Enter a string: ", (text) => {
-  const reversed = reverseUsingArrayMethods(text);
-  console.log(`Reversed string: ${reversed}`);
-  rl.close();
-});
+console.log(rev);
