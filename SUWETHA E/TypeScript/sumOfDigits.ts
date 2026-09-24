42dc63e36d409d4ff05f8a3d81397e9ebcbd1566
Suwetha E <suwethas71@gmail.com>
2026-09-24 14:17:59 +0530

    Change input method for sumOfDigits function
    Refactor sumOfDigits function to read input from a file instead of user input.

--- SUWETHA E/TypeScript/sumOfDigits.ts
+++ SUWETHA E/TypeScript/sumOfDigits.ts
@@ -1,28 +1,11 @@
-import * as readline from "readline";
+import * as fs from "fs";
 
-export function sumOfDigits(num: number): number {
-  let n = Math.abs(Math.floor(num));
-  let sum = 0;
+let n = Number(fs.readFileSync(0, "utf8"));
+let sum = 0;
 
-  while (n > 0) {
+while (n > 0) {
     sum += n % 10;
     n = Math.floor(n / 10);
-  }
-
-  return sum;
 }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-rl.question("Enter an integer: ", (answer: string) => {
-  const parsedNum = parseInt(answer, 10);
-  if (isNaN(parsedNum)) {
-    console.log("Please enter a valid numeric value.");
-  } else {
-    console.log(`Sum of digits of ${parsedNum} is: ${sumOfDigits(parsedNum)}`);
-  }
-  rl.close();
-});
+console.log(sum);
