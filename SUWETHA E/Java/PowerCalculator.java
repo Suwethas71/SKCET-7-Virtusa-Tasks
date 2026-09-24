1384feda7392eed6015d5eebd6283863ba1dc5d6
Suwetha E <suwethas71@gmail.com>
2026-09-24 13:59:01 +0530

    Refactor PowerCalculator for double input and negative exponents
    Updated variable type for base and adjusted loop for exponentiation.

--- SUWETHA E/Java/PowerCalculator.java
+++ SUWETHA E/Java/PowerCalculator.java
@@ -4,13 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
+        double a = sc.nextDouble();
         int b = sc.nextInt();
 
         double result = 1;
 
-        for (int i = 1; i <= b; i++)
+        for (int i = 0; i < Math.abs(b); i++)
             result *= a;
+
+        if (b < 0)
+            result = 1 / result;
 
         System.out.println(result);
     }
