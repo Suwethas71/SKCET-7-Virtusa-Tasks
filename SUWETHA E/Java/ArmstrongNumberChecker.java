3e6003fa4e8265eefb5c984b461dc5c7bc8f4c28
Suwetha E <suwethas71@gmail.com>
2026-09-24 13:52:21 +0530

    Refactor Armstrong number checker implementation

--- SUWETHA E/Java/ArmstrongNumberChecker.java
+++ SUWETHA E/Java/ArmstrongNumberChecker.java
@@ -1,41 +1,28 @@
-import java.util.Scanner;
-public class ArmstrongNumberChecker {
-    private static int power(int base, int exp) {
-        int result = 1;
-        for (int i = 0; i < exp; i++) {
-            result *= base;
-        }
-        return result;
-    }
-    public static boolean isArmstrong(int number) {
-        if (number < 0) {
-            return false;
-        }
-        int temp = number;
-        int digitCount = 0;
-        while (temp != 0) {
-            digitCount++;
-            temp /= 10;
-        }
-        temp = number;
-        int sum = 0;
-        while (temp != 0) {
-            int digit = temp % 10;
-            sum += power(digit, digitCount);
-            temp /= 10;
-        }
-        return sum == number;
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
-        int inputNumber = scanner.nextInt();
+import java.util.*;
 
-        if (isArmstrong(inputNumber)) {
-            System.out.println(inputNumber + " is an Armstrong number.");
-        } else {
-            System.out.println(inputNumber + " is not an Armstrong number.");
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int temp = n, sum = 0, digits = 0;
+
+        while (temp > 0) {
+            digits++;
+            temp /= 10;
         }
-        scanner.close();
+
+        temp = n;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += Math.pow(digit, digits);
+            temp /= 10;
+        }
+
+        if (sum == n)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
     }
 }
